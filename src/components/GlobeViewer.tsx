fea5d17699675c5a28abad03285d3cd8627cd217
rasesh <raseshrout@gmail.com>
2024-09-13 00:37:18 +0530

    change to view all top artist countries

--- src/components/GlobeViewer.tsx
+++ src/components/GlobeViewer.tsx
@@ -1,24 +1,27 @@
 import ArtistsGlobe from './ArtistsGlobe'
 import { useCallback, useEffect, useRef, useState } from 'react'
-// import { useSoundglobeBackend } from '../hooks/useSoundglobeBackend'
-import { Country, Point } from '../constants'
+import { useSoundglobeBackend } from '../hooks/useSoundglobeBackend'
+import { Country, Point, CountryArtistData } from '../constants'
 import { useAppContext } from '../hooks/useAppContext'
+import { v4 as uuidv4 } from 'uuid'
 
 type GlobeViewerProps = {
     countries: Country[]
     targetCountryCenter: Point
     targetCountry: string
     targetCountryCount: number
+    allCountriesArtistData: CountryArtistData[]
+    topArtistsCountries: Set<string>
 }
 
-const GlobeViewer = ({countries, targetCountryCenter, targetCountry, targetCountryCount}: GlobeViewerProps) => {
+const GlobeViewer = ({countries, targetCountryCenter, targetCountry, targetCountryCount, allCountriesArtistData, topArtistsCountries}: GlobeViewerProps) => {
 
     const globeDivRef = useRef<HTMLDivElement>(null)
     const [canvasSize, setCanvasSize] = useState(window.innerWidth > 500 ? 500 : window.innerWidth)
-    // const [shareableLink, setShareableLink] = useState('')
-    // const { addImage } = useSoundglobeBackend()
+    const [shareableLink, setShareableLink] = useState('')
+    const { addImage } = useSoundglobeBackend()
     const { globePOV, appGlobeRef } = useAppContext()
-    // const [isCreatingLink, setIsCreatingLink] = useState(false)
+    const [isCreatingLink, setIsCreatingLink] = useState(false)
 
     const adjustGlobePointOfView = useCallback(() => {
         if (!appGlobeRef.current) return
@@ -31,30 +34,31 @@ const GlobeViewer = ({countries, targetCountryCenter, targetCountry, targetCount
         }
     }, [globePOV, appGlobeRef]);
 
-    // const openTwitterToPost = (url: string) => {
-    //     window.open(`https://twitter.com/intent/tweet?url=${url}`)
-    // }
+    const openTwitterToPost = (url: string) => {
+        window.open(`https://twitter.com/intent/tweet?url=${url}`)
+    }
 
-    // const writeImage = async (email: string, imageData: string) => {
-    //     const { success, id } = await addImage(email, imageData)
-    //     if (success) {
-    //         const url = import.meta.env.VITE_BACKEND_URL + "/image/" + id
-    //         setShareableLink(url)
-    //         openTwitterToPost(url)
-    //     }
-    // }
+    const writeImage = async (uid: string, imageData: string) => {
+        const { success, id } = await addImage(uid, imageData)
+        if (success) {
+            const url = import.meta.env.VITE_BACKEND_URL + "/image/" + id
+            setShareableLink(url)
+            openTwitterToPost(url)
+        }
+    }
 
     const handleShare = () => {
-        // if (!appGlobeRef.current) return
-        // if (shareableLink) {
-        //     openTwitterToPost(shareableLink)
-        //     return
-        // }
-        // setIsCreatingLink(true)
-        // const renderer = appGlobeRef.current.renderer()
-        // const imageDataFromGlobe: string = renderer.domElement.toDataURL('image/jpeg', 0.5)
+        if (!appGlobeRef.current) return
+        if (shareableLink) {
+            openTwitterToPost(shareableLink)
+            return
+        }
+        setIsCreatingLink(true)
+        const renderer = appGlobeRef.current.renderer()
+        const imageDataFromGlobe: string = renderer.domElement.toDataURL('image/jpeg', 0.5)
+        const uid = uuidv4()
 
-        // writeImage(userEmail, imageDataFromGlobe).finally(() => setIsCreatingLink(false))
+        writeImage(uid, imageDataFromGlobe).finally(() => setIsCreatingLink(false))
     }
 
     useEffect(() => {
@@ -78,24 +82,25 @@ const GlobeViewer = ({countries, targetCountryCenter, targetCountry, targetCount
     useEffect(() => {
         if (globePOV && globePOV.latitude) {
             adjustGlobePointOfView()
-            // console.log('globePOV change useEffect')
+            console.log('globePOV change useEffect')
         }
     }, [globePOV, adjustGlobePointOfView])
 
     return (
         <div className="h-screen w-screen flex flex-col justify-center items-center bg-black gap-8">
             <div className="text-white text-2xl font-bold">
-                Your music map
+                Your top artists country map!
             </div>
             <div className="w-full aspect-square sm:max-w-[500px] bg-black" ref={globeDivRef}>
                 <ArtistsGlobe countries={countries} targetCountryCenter={targetCountryCenter} 
                     targetCountry={targetCountry} targetCountryCount={targetCountryCount} 
                     adjustGlobePointOfView={adjustGlobePointOfView} canvasSize={canvasSize}
+                    allCountriesArtistData={allCountriesArtistData} topArtistsCountries={topArtistsCountries}
                 />
             </div>
-            <div className={`bg-black text-white px-8 py-2 rounded-full border-2 border-white font-bold hover:cursor-pointer `} onClick={handleShare}> {/**${isCreatingLink ? 'opacity-70' : ''} */}
-                {/* {isCreatingLink ? 'Creating link...' : 'Share with friends'} */}
-                Share with friends
+            <div className={`bg-black text-white px-8 py-2 rounded-full border-2 border-white font-bold hover:cursor-pointer ${isCreatingLink ? 'opacity-70' : ''}`} onClick={handleShare}> {/**${isCreatingLink ? 'opacity-70' : ''} */}
+                {isCreatingLink ? 'Creating link...' : 'Share with friends'}
+                {/* Share with friends */}
             </div>
             {/* {shareableLink} */}
         </div>
